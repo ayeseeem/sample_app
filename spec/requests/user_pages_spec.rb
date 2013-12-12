735dbc8a9c1a156e602d094e0a14056ad9eaa465
ayeseeem <ayeseeem@gmail.com>
2013-12-12 09:40:44 +0000

    Test signup error page - Ex 7.6 -2

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,6 +29,18 @@ describe "UserPages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+        # don't test error messages to specifically?
+        #it { should have_content("Name can't be blank") }
+        #it { should have_content("Email can't be blank") }
+        #it { should have_content("Email is invalid") }
+        #it { should have_content("Password can't be blank") }
+      end
     end
 
     describe "with valid information" do
